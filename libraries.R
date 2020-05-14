fb05deff8aa8c4a0bf126725378670c97312d45d
Julian Tagell <julian@tadge-analytics.com.au>
2020-05-14 14:27:47 +1000

    a small change

--- libraries.R
+++ libraries.R
@@ -5,3 +5,13 @@ library(plotly)
 library(DT)
 library(openxlsx)
 library(shinyWidgets)
+
+# a little change
+
+# Run
+# 
+#   git config --global user.email "you@example.com"
+#   git config --global user.name "Your Name"
+
+
+# usethis::use_git_config(user.name = "Julian Tagell", user.email = "julian@tadge-analytics.com.au")
